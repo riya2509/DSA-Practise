ea97eb47235406d60d227d0c6782fa2bd73a787b
riya2509 <91462077+riya2509@users.noreply.github.com>
2023-10-22 18:51:30 +0530

    Basic array user input

--- Test/Main.java
+++ Test/Main.java
@@ -1,6 +1,17 @@
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        // int[] myArray = { 1, 2, 3 };
+        // System.out.println("Hello" + myArray);
+        int[] a = new int[5];
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter array elements");
+        for (int i = 0; i < 5; i++)
+            a[i] = sc.nextInt();
+        System.out.println("Array elements are:");
+        for (int j = 0; j < a.length; j++)
+            System.out.println(a[j]);
+
     }
 }
