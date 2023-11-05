9548dbfd4a73b5d23f029229101e1972cb79301d
riya2509 <91462077+riya2509@users.noreply.github.com>
2023-11-05 20:23:49 +0530

    Refactoring

---
+++ Test/2.java
@@ -0,0 +1,10 @@
+// Kadane's Algorithm
+
+// Given an array arr of N integers, find the contiguous sub-array with the
+// maximum sum.
+// Example:
+// Input:
+// N = 5
+// Arr[ ] = {1, 2, 3, -2, 5}
+// Output:
+// 9
