75322a7a9ee5b4833c44824a86ea8235639ac0ab
riya2509 <91462077+riya2509@users.noreply.github.com>
2023-12-09 12:26:59 +0530

    Linked List Creation and traversal

---
+++ Basics/LinkedList/Main.java
@@ -0,0 +1,35 @@
+package Basics.LinkedList;
+
+class Node {
+
+    int data;
+    Node next;
+
+    Node(int data) {
+        this.data = data;
+    }
+
+}
+
+class Main {
+    public static void main(String args[]) {
+        Node n1 = new Node(10);
+        Node n2 = new Node(20);
+        Node n3 = new Node(30);
+
+        Node head = n1;
+        head.next = n2;
+        n2.next = n3;
+        n3.next = null;
+
+        Traverse(head);
+    }
+
+    static void Traverse(Node head) {
+        Node cur = head;
+        while (cur != null) {
+            System.out.println(cur.data);
+            cur = cur.next;
+        }
+    }
+}
