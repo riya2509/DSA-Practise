460a7a6b2a428674fbd34b6e99deccd9bd141d2d
riya2509 <91462077+riya2509@users.noreply.github.com>
2024-05-18 11:05:18 +0530

    Appending LinkList

--- LinkedListLatest/LinkedList.java
+++ LinkedListLatest/LinkedList.java
@@ -49,4 +49,16 @@ public class LinkedList {
     public void getLength() {
         System.out.println("Length:" + length);
     }
+
+    public void append(int value) {
+        Node newNode = new Node(value);
+        if (length == 0) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail.next = newNode;
+            tail = newNode;
+        }
+        length++;
+    }
 }
--- LinkedListLatest/Main.java
+++ LinkedListLatest/Main.java
@@ -3,11 +3,10 @@ package LinkedListLatest;
 public class Main {
     public static void main(String args[]) {
         LinkedList myLinkedList = new LinkedList(4);
-
+        myLinkedList.append(1);
         myLinkedList.getHead();
         myLinkedList.getTail();
         myLinkedList.getLength();
         myLinkedList.printList();
-
     }
 }
