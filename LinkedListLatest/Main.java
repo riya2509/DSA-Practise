fe5382e298d445f4790e827656d0fed64d198c72
riya2509 <91462077+riya2509@users.noreply.github.com>
2024-05-18 13:24:10 +0530

    Prepend method

--- LinkedListLatest/Main.java
+++ LinkedListLatest/Main.java
@@ -4,13 +4,17 @@ public class Main {
     public static void main(String args[]) {
         LinkedList myLinkedList = new LinkedList(4);
         myLinkedList.append(1);
-        myLinkedList.getHead();
-        myLinkedList.getTail();
-        myLinkedList.getLength();
-        // myLinkedList.printList();
-        System.out.println(myLinkedList.removeLast().value); // for 2 items and returns Node so use .value
-        System.out.println(myLinkedList.removeLast().value);// for 1 item
-        System.out.println(myLinkedList.removeLast()); // for 0 item and returns null so null.value not correct
+        // myLinkedList.getHead();
+        // myLinkedList.getTail();
+        // myLinkedList.getLength();
+        myLinkedList.prepend(5);
+        myLinkedList.printList();
+
+        // System.out.println(myLinkedList.removeLast().value); // for 2 items and
+        // returns Node so use .value
+        // System.out.println(myLinkedList.removeLast().value);// for 1 item
+        // System.out.println(myLinkedList.removeLast()); // for 0 item and returns null
+        // so null.value not correct
 
     }
 }
