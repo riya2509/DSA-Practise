bb9700df40b9c5f9ca13cbb96dd6b0fe6e7e5e58
riya2509 <91462077+riya2509@users.noreply.github.com>
2024-05-18 13:39:11 +0530

    Removing first node from LL

--- LinkedListLatest/LinkedList.java
+++ LinkedListLatest/LinkedList.java
@@ -96,4 +96,18 @@ public class LinkedList {
         }
         length++;
     }
+
+    public Node removeFirst() {
+        if (length == 0)
+            return null;
+        Node temp = head;
+        head = head.next;
+        temp.next = null;
+        length--;
+        if (length == 0) {
+            // head = null not needed as head already null in case of one item
+            tail = null;
+        }
+        return temp;
+    }
 }
--- LinkedListLatest/Main.java
+++ LinkedListLatest/Main.java
@@ -7,14 +7,12 @@ public class Main {
         // myLinkedList.getHead();
         // myLinkedList.getTail();
         // myLinkedList.getLength();
-        myLinkedList.prepend(5);
-        myLinkedList.printList();
+        // myLinkedList.prepend(5);
+        // myLinkedList.printList();
 
-        // System.out.println(myLinkedList.removeLast().value); // for 2 items and
-        // returns Node so use .value
-        // System.out.println(myLinkedList.removeLast().value);// for 1 item
-        // System.out.println(myLinkedList.removeLast()); // for 0 item and returns null
-        // so null.value not correct
+        System.out.println(myLinkedList.removeFirst().value); // for 2 items and returns Node so use .value
+        System.out.println(myLinkedList.removeFirst().value);// for 1 item
+        System.out.println(myLinkedList.removeFirst()); // for 0 item and returns null so null.value not correct
 
     }
 }
