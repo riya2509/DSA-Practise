d5682cfd005b3c18d97b5adbba8e52821b6d1ef6
riya2509 <91462077+riya2509@users.noreply.github.com>
2024-05-19 11:04:18 +0530

    Inserting an element in a LL

--- LinkedListLatest/Main.java
+++ LinkedListLatest/Main.java
@@ -3,16 +3,17 @@ package LinkedListLatest;
 public class Main {
     public static void main(String args[]) {
         LinkedList myLinkedList = new LinkedList(0);
-        myLinkedList.append(1);
+        // myLinkedList.append(1);
         myLinkedList.append(2);
-        myLinkedList.append(3);
+        // myLinkedList.append(3);
 
         // myLinkedList.getHead();
         // myLinkedList.getTail();
         // myLinkedList.getLength();
         // myLinkedList.prepend(5);
         // System.out.println(myLinkedList.get(2).value + "\n");
-        myLinkedList.set(1, 4);
+        // myLinkedList.set(1, 4);
+        myLinkedList.insert(1, 1);
         myLinkedList.printList();
 
         // System.out.println(myLinkedList.removeFirst().value); // for 2 items and
