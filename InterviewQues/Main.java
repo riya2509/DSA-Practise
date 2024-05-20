72332e930e7c9f2f359007a1add6362b70def333
riya2509 <91462077+riya2509@users.noreply.github.com>
2024-05-20 20:14:35 +0530

    Refactoring

--- InterviewQues/Main.java
+++ InterviewQues/Main.java
@@ -21,14 +21,18 @@ public class Main {
     // return false;
     // }
     public static boolean itemInCommon(int[] array1, int[] array2) {
+        // Create a HashMap to store the elements of array1
         HashMap<Integer, Boolean> myHashMap = new HashMap<>();
+        // Add each element of array1 to the HashMap with a true value
         for (int i : array1) {
             myHashMap.put(i, true);
         }
+        // Check if each element of array2 is in the HashMap
         for (int j : array2) {
             if (myHashMap.get(j) != null)
                 return true;
         }
+        // If no elements in common, return false
         return false;
     }
 
