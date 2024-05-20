addadbac86d0891da9c7b82d3ff6a86d487133b4
riya2509 <91462077+riya2509@users.noreply.github.com>
2024-05-20 10:29:26 +0530

    Merging Two sorted lists using recursion

--- LinkedListLatest/LinkedList.java
+++ LinkedListLatest/LinkedList.java
@@ -220,4 +220,28 @@ public class LinkedList {
         return false;
     }
 
+    // Merging Two sorted lists using recursion
+    public Node mergeTwoLists(Node list1, Node list2) {
+        if (list1 == null)
+            return list2;
+        if (list2 == null)
+            return list1;
+        if (list1.value < list2.value) {
+            list1.next = mergeTwoLists(list1.next, list2);
+            return list1;
+        } else {
+            list2.next = mergeTwoLists(list1, list2.next);
+            return list2;
+        }
+    }
+
+    // declared static so that it can be called without an instance of 'LinkedList'
+    public static void printList(Node head) {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.value + " -> ");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
 }
--- LinkedListLatest/Main.java
+++ LinkedListLatest/Main.java
@@ -2,10 +2,10 @@ package LinkedListLatest;
 
 public class Main {
     public static void main(String args[]) {
-        LinkedList myLinkedList = new LinkedList(11);
-        myLinkedList.append(3);
-        myLinkedList.append(23);
-        myLinkedList.append(7);
+        // LinkedList myLinkedList = new LinkedList(11);
+        // myLinkedList.append(3);
+        // myLinkedList.append(23);
+        // myLinkedList.append(7);
 
         // myLinkedList.getHead();
         // myLinkedList.getTail();
@@ -19,15 +19,34 @@ public class Main {
         // System.out.println(myLinkedList.findMiddleNode().value);
         // myLinkedList.printList();
 
-        myLinkedList.getTail().next = myLinkedList.getHead().next;
-        System.out.println("Has Loop:");
-        System.out.println(myLinkedList.hasLoop());
+        // myLinkedList.getTail().next = myLinkedList.getHead().next;
+        // System.out.println("Has Loop:");
+        // System.out.println(myLinkedList.hasLoop());
 
         // System.out.println(myLinkedList.removeFirst().value); // for 2 items and
         // returns Node so use .value
         // System.out.println(myLinkedList.removeFirst().value);// for 1 item
         // System.out.println(myLinkedList.removeFirst()); // for 0 item and returns
         // null so null.value not correct
+
+        LinkedList list1 = new LinkedList(1);
+        list1.append(3);
+        list1.append(4);
+        list1.append(7);
+
+        LinkedList list2 = new LinkedList(1);
+        list2.append(2);
+        list2.append(5);
+        list2.append(10);
+        System.out.println("List 1:");
+        list1.printList();
+        System.out.println("List 2:");
+        list2.printList();
+
+        LinkedList.Node mergedHead = list1.mergeTwoLists(list1.getHead(), list2.getHead());// Merge two lists
+        // Print the merged list
+        System.out.println("Merged List:");
+        LinkedList.printList(mergedHead);
 
     }
 }
