14b56c09dc5a4d86aaee9296f35f014c48d8aa85
riya2509 <91462077+riya2509@users.noreply.github.com>
2024-05-25 03:04:36 +0530

    Queue constructor

---
+++ queue/Main.java
@@ -0,0 +1,9 @@
+package queue;
+
+public class Main {
+
+    public static void main(String args[]) {
+        Queue myQueue = new Queue(7);
+        myQueue.printQueue();
+    }
+}
---
+++ queue/Queue.java
@@ -0,0 +1,32 @@
+package queue;
+
+public class Queue {
+    private Node first;
+    private Node last;
+    private int length;
+
+    class Node {
+        int value;
+        Node next;
+
+        Node(int value) {
+            this.value = value;
+        }
+    }
+
+    public Queue(int value) {
+        Node newNode = new Node(value);
+        first = newNode;
+        last = newNode;
+        length = 1;
+    }
+
+    public void printQueue() {
+        Node temp = first;
+        while (temp != null) {
+            System.out.print(temp.value);
+            temp = temp.next;
+        }
+    }
+
+}
