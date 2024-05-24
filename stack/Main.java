3a8c91ac15594b821417e1abc4b8b086947dc351
riya2509 <91462077+riya2509@users.noreply.github.com>
2024-05-24 18:41:39 +0530

    Stack push operation

--- stack/Main.java
+++ stack/Main.java
@@ -4,8 +4,9 @@ public class Main {
     public static void main(String args[]) {
         Stack myStack = new Stack(4);
 
-        myStack.getTop();
-        myStack.getHeight();
+        // myStack.getTop();
+        // myStack.getHeight();
+        myStack.push(8);
         myStack.printStack();
     }
 }
