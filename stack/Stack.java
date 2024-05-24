c98924d0477afa2c3ba7e0a0cc75f50031dae472
riya2509 <91462077+riya2509@users.noreply.github.com>
2024-05-24 18:32:46 +0530

    Stack Constructor

---
+++ stack/Stack.java
@@ -0,0 +1,39 @@
+package stack;
+
+public class Stack {
+
+    private Node top;
+    private int height;
+
+    class Node {
+        int value;
+        Node next;
+
+        Node(int value) {
+            this.value = value;
+        }
+    }
+
+    public Stack(int value) {
+        Node newNode = new Node(value);
+        top = newNode;
+        height = 1;
+    }
+
+    public void printStack() {
+        Node temp = top;
+        while (temp != null) {
+            System.out.print(temp.value);
+            temp = temp.next;
+        }
+
+    }
+
+    public void getTop() {
+        System.out.println("Top : " + top.value);
+    }
+
+    public void getHeight() {
+        System.out.println("Height : " + height);
+    }
+}
