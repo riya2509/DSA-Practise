6428a37e2856dc4bdada1f918865de90cc523d86
riya2509 <91462077+riya2509@users.noreply.github.com>
2024-05-26 23:47:36 +0530

    Practice

---
+++ Test/LinkedList.java
@@ -0,0 +1,24 @@
+
+public class LinkedList {
+    private Node head;
+    private Node tail;
+    private int length;
+
+    class Node {
+        int value;
+        Node next;
+
+        Node(int value) {
+            this.value = value;
+        }
+    }
+
+    // Constructor
+    public LinkedList(int value) {
+        Node newNode = new Node(value);
+        head = newNode;
+        tail = newNode;
+        length = 1;
+
+    }
+}
