cf6bda6589e0686157ea91b0efde6ee00a2d16ef
riya2509 <91462077+riya2509@users.noreply.github.com>
2024-05-27 21:03:48 +0530

    Practice

--- Test/LinkedList.java
+++ Test/LinkedList.java
@@ -21,4 +21,23 @@ public class LinkedList {
         length = 1;
 
     }
+
+    public Node findKthFromEnd(int k) {
+        Node slow = head;
+        Node fast = head;
+
+        for (int i = 0; i < k; i++) {
+            if (fast == null) {
+                return null;
+            }
+            fast = fast.next;
+        }
+
+        while (fast != null) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+
+        return slow;
+    }
 }
