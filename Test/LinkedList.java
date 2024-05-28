5157b481ecf8a03127561b1e07b20a639fa0459e
riya2509 <91462077+riya2509@users.noreply.github.com>
2024-05-28 19:44:16 +0530

    practice

--- Test/LinkedList.java
+++ Test/LinkedList.java
@@ -40,4 +40,18 @@ public class LinkedList {
 
         return slow;
     }
+
+    public Node mergeTwoLists(Node list1, Node list2) {
+        if (list1 == null)
+            return list2;
+        if (list2 == null)
+            return list1;
+        if (list1.value < list2.value) {
+            list1.next = mergeTwoLists(list1.next, list2);
+            return list1;
+        } else {
+            list2.next = mergeTwoLists(list1, list2.next);
+            return list2;
+        }
+    }
 }
